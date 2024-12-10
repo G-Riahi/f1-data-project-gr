766ffb898118e45aabc80cf6679ca318c9dd765d
G-Riahi <griahi000@gmail.com>
2024-12-10 14:11:46 +0000

    modifying the dataset extraction script

--- pull-dataset.sh
+++ pull-dataset.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 DATASET_REPO="/home/floppabox/f1/f1db"
+GIT_DATASET="https://github.com/f1db/f1db.git"
+
+
 if [ -d "$DATASET_REPO" ]; then
 	cd "$DATASET_REPO" || exit
+	echo "--Updating dataset--"
+	git pull origin main
+	if [ $? -eq 0 ]; then
+		echo "--Dataset updated successfully--"
+	else
+		echo "--Error: update failed--"
+		exit 1
+	fi
 else
-	echo "--Error: no directory as such--"
-	exit 1
-fi
-
-echo "--Updating dataset--"
-git pull origin main
-
-if [ $? -eq 0 ]; then
-	echo "--Dataset updated successfully--"
-else
-	echo "--Error: update failed--"
-	exit 1
+	echo "--Cloning Dataset--"
+	mkdir -p "$DATASET_REPO"
+	git clone "$GIT_DATASET" "$DATASET_REPO"
+	echo "--Dataset cloned successfully--"
 fi
