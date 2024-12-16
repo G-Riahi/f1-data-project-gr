dc32895bdc5912dc2740a2015f1072c07f84533f
G-Riahi <griahi000@gmail.com>
2024-12-16 15:13:39 +0000

    cleaning and optimizing the drivers transformation script (failes paralellization process)

--- python_scripts/DriversToCSV.py
+++ python_scripts/DriversToCSV.py
@@ -11,54 +11,53 @@ import os
 #########################################################
 
 
+
 # Getting all possible keys in the drivers data
 
-def keys_list(folder_path):
-    max_length = 0
-    keys = []
-    for file_name in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, file_name)
+def keys_list(folderPath):
+    fileNames = [file for file in os.listdir(folderPath)]
+    keysSeen = {}
+
+    for fileName in fileNames:
+        file_path = os.path.join(folderPath, fileName)
         with open(file_path, 'r') as file:
             data=yaml.safe_load(file)
-            if max_length<=len(list(data.keys())):
-                max_length = len(list(data.keys()))
-                keys = list(data.keys())
+            for key in data.keys():
+                if key not in keysSeen:
+                    keysSeen[key]=None
 
-    return keys
+    return list(keysSeen.keys()), fileNames
 
 # Extracting all drivers data
 
-def transformData(folder_path):
+def transformData(folderPath):
 
-    keys = keys_list(folder_path)
+    keys, filesNames = keys_list(folderPath)
     drivers_data, drivers_relationships_data = [], []
+    filesPaths = [os.path.join(folderPath, fileName) for fileName in filesNames]
     id = 0
 
-    for file_name in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, file_name)
+    for filePath in filesPaths:
 
-        with open(file_path, 'r') as file:
+        with open(filePath, 'r') as file:
             content_drivers=yaml.safe_load(file)
-            record ={}
 
-            for key in keys:
-                if key != 'familyRelationships':
-                    record[key]= content_drivers.get(key)
-
-            drivers_data.append(record)
+            record_driver ={key: content_drivers[key] for key in keys if key != 'familyRelationships'}
+            drivers_data.append(record_driver)
             
             if 'familyRelationships' in content_drivers.keys():
                 for rel in content_drivers['familyRelationships']:
-                    record ={}
-                    record['id']=id
-                    id=id+1
-                    record['driverId']=content_drivers.get('id')
-                    record['relationId']=rel.get('driverId')
-                    record['type']=rel.get('type')
-                    drivers_relationships_data.append(record)
+                    record_relation = {
+                        'id': id,
+                        'driverId' : content_drivers.get('id'),
+                        'relationId' : rel.get('driverId'),
+                        'type' : rel.get('type')
+                    }
+                    id+=1
+                    drivers_relationships_data.append(record_relation)
 
     
-    return drivers_data, drivers_relationships_data
+    return drivers_data, drivers_relationships_data, keys
 
 
 # Creating spark dataframes
@@ -67,8 +66,7 @@ def driversToSpark(folder_path="/home/floppabox/f1/f1db/src/data/drivers" , fold
 
     spark = SparkSession.builder.appName(appName).getOrCreate()
 
-    drivers_data, drivers_relationships_data = transformData(folder_path)
-    keys = keys_list(folder_path)
+    drivers_data, drivers_relationships_data, keys = transformData(folder_path)
     drivers = spark.createDataFrame(drivers_data).select([x for x in keys if x != 'familyRelationships'])
     drivers_relationships = spark.createDataFrame(drivers_relationships_data).select(['id','driverId', 'relationId', 'type'])
 
